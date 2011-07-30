a920a29f0aa65ccda3e5ac3b2e7cbfb1991b4c76
Luca Ongaro <lukeongaro@gmail.com>
2011-07-30 15:38:49 +0200

    add -l option to command s

--- src/commands.js
+++ src/commands.js
@@ -9,10 +9,16 @@ JSterminal.register("tweet", {
 });
 
 JSterminal.register("s", {
+  options: {
+    "-d": {
+      argument: true,
+      description: "top level domain to use (e.g. 'com', 'de' or 'it'). Default is 'com'."
+    }
+  },
   description: "search on Google",
   help: "it searches the string passed as argument on Google\nSynopsis:\n  s SEARCH_QUERY",
-  execute: function(argv){
-    window.open("http://www.google.com/search?q="+argv.join("+"));
+  execute: function(argv, options){
+    window.open("http://www.google."+(options["-d"] || "com")+"/search?q="+argv.join("+"));
   }
 });
 
@@ -28,7 +34,7 @@ JSterminal.register("w", {
   options: {
     "-l": {
       argument: true,
-      description: "language (e.g. 'en' or 'it')"
+      description: "language code (e.g. 'en' or 'it')"
     }
   },
   description: "search on Wikipedia",
