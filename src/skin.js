46895f46d410ea5cb226aef941f41dc5b668f37f
Luca Ongaro <lukeongaro@gmail.com>
2011-08-03 00:45:15 +0200

    fixes

--- src/skin.js
+++ src/skin.js
@@ -21,6 +21,9 @@ JSterminal.ioQueue = (function() { // Queue of IO interfaces that claimed contro
     },
     noneActive: function() {
       return typeof this.firstActive() == "undefined";
+    },
+    empty: function() {
+      queue = [];
     }
   }
 })();
@@ -56,11 +59,11 @@ JSterminal.IO = function(opts) {
       }
     },
     gets: function(callback) {
-      jQuery("#JSterminal_in_prefix").html(this.meta.prefixes.input || "");
+      if (!this.active) {
+        this.claim();
+        this.immediateRelease = true;
+      }
       this.meta.getsCallbacks.push(callback);
-    },
-    flush: function(out) {
-      jQuery("#JSterminal_out").html("");
     },
     claim: function() {
       this.active = true;
@@ -82,44 +85,44 @@ JSterminal.IO = function(opts) {
 // Terminal input/output interface
 JSterminal.meta.termIO = JSterminal.IO();
 
-JSterminal.meta.printWaitingOutput = function() {
-  var io = JSterminal.ioQueue.firstActive();
-  if(!!io) {
-    for (out in io.outputQueue) {
-      jQuery("#JSterminal_in_wrap").before((this.meta.prefixes.output || "")+(io.outputQueue[out]||"")+"\n");
-      jQuery("#JSterminal_out").scrollTop(jQuery("#JSterminal_out").attr("scrollHeight"));
-      jQuery("#JSterminal_in").focus();
-    }
-  }
-}
-
 // Handle ioReleased
 JSterminal.eventHandlers.ioReleased = function() {
-  JSterminal.meta.printWaitingOutput();
+  var io = JSterminal.ioQueue.firstActive();
+  if(!!io) {
+    jQuery("#JSterminal_in_prefix").html(io.meta.prefixes.input || "");
+    var out;
+    while (out = io.meta.outputQueue.shift()) {
+      jQuery("#JSterminal_in_wrap").before((io.meta.prefixes.output || "")+(out||"")+"\n");
+      jQuery("#JSterminal_out").scrollTop(jQuery("#JSterminal_out").attr("scrollHeight"));
+      jQuery("#JSterminal_in").focus();
+      if (io.immediateRelease) {
+        io.release();
+      }
+    }
+  }
 }
 
 // Handle onkeydown event in #JSterminal_in
 JSterminal.eventHandlers.keyPressed = function(e) {
   e = e || window.event;
   var keycode = e.keyCode || e.which;
-  var termIO = JSterminal.meta.termIO;
-  if (JSterminal.ioQueue.noneActive()) {
-    termIO.claim();
-    termIO.gets(function(s) {
-      termIO.puts(s);
-      termIO.release();
-      JSterminal.interpret(s);
-    });
-  }
   // Handle input in the right scope
+  var termIO = JSterminal.meta.termIO;
+  termIO.gets(function(s) {
+    termIO.puts(s);
+    JSterminal.interpret(s);
+  });
+  JSterminal.eventHandlers.ioReleased();
   var io = JSterminal.ioQueue.firstActive();
   if(keycode === 13) {
-    jQuery("#JSterminal_in_prefix").html("");
     var i = jQuery("#JSterminal_in").val();
     jQuery("#JSterminal_in").val("");
     io.meta.inputLog.unshift(i);
     io.meta.inputLogCursor = -1;
     io.meta.getsCallbacks.shift()(i);
+    if (io.immediateRelease) {
+      io.release();
+    }
   } else if (keycode === 27) {
       JSterminal.quit();
   } else if (keycode === 38) {
@@ -139,14 +142,12 @@ JSterminal.launch = function() {
   if(jQuery("#JSterminal_container").length <= 0)
   {
     jQuery(document.body).prepend('<div id="JSterminal_container"><style type="text/css">#JSterminal_container {background: #000; background: rgba(0, 0, 0, 0.9); overflow: visible; padding: 0; margin: 0; z-index: 50000; position: fixed; top: 15px; left: 50%; margin-left: -300px; width: auto; -moz-box-shadow: 2px 10px 50px #000; box-shadow: 2px 10px 100px #000; border: 1px solid #aaa; font-size: 12px; line-height: 120%;} #JSterminal_out {background: transparent; color: #0d0; padding: 1em; white-space: pre-wrap; font-family: Courier New, Courier, monospace; font-size: 14px; text-align: left; width: 600px; height: 400px; overflow-y: auto;} #JSterminal_in_wrap {margin: 0; padding: 0;} #JSterminal_in {width: 560px; background: #000; background: rgba(0, 0, 0, 0.9); border: none; color: #0d0; font-family: Courier New, Courier, monospace; font-size: 1em; outline-style: none;} #JSterminal_close {position: absolute; top: -8px; left: -8px; border: 2px solid #fff; background: #a00; -moz-border-radius: 16px; border-radius: 16px; width: 16px; height: 16px; overflow: hidden; text-align: center; -moz-box-shadow: 1px 2px 5px #000; box-shadow: 1px 2px 5px #333;} #JSterminal_close span:before { vertical-align: middle; color: #fff; font-weight: normal; text-decoration: none; content: "\\D7"; font-size: 20px; font-family: Courier new, monospace; line-height: 17px;}</style><div id="JSterminal_close" onclick="JSterminal.interpret(\'exit\'); return false;"><span></span></div><div id="JSterminal_out" onclick="jQuery(\'#JSterminal_in\').focus()"><div id="JSterminal_in_wrap"><span id="JSterminal_in_prefix"></span><input type="text" id="JSterminal_in" onkeydown="return JSterminal.eventHandlers.keyPressed(event)" /></div></div></div>');
-    JSterminal.meta.termIO.claim();
     JSterminal.meta.termIO.puts("JSterminal version 0.0.3\n"+(new Date()).toLocaleString());
-    JSterminal.meta.termIO.release();
     jQuery("#JSterminal_in").focus();
   }
 };
 
 JSterminal.quit = function() {
-  JSterminal.meta.inputQueue = [];
+  JSterminal.ioQueue.empty();
   jQuery("#JSterminal_container").remove();
 }
